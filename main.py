3b9b77792ffcdcb8df8b8e618c8a81b805f334b4
Arsbul-hub <ars.bulygin@yandex.ru>
2025-04-17 10:42:45 +0300

    Необходимы тесты. Реализован автоподбор радиуса (без провеки на начало и конец спска - новое условие)

--- main.py
+++ main.py
@@ -1,77 +1,76 @@
 import pandas as pd
 from openpyxl import load_workbook
 import random
+from numba import jit
 
-sheets = pd.read_excel('data.xlsx', sheet_name=None)
+sheets = pd.read_excel('data.xlsx')
+data = pd.DataFrame(sheets, columns=["Название судна", 'ФИО уастников'])
+original_data = data.values.tolist()
 out = []
 
 
 def check_collide(output_list, ship_name, people_list, min_distance):
+    if not output_list:
+        return False
     for i in range(len(output_list) - 1, -1, -1):
         ship, pp = output_list[i]
+        pp = pp.split(";")
         if ship == ship_name:
             rad = len(output_list) - i - 1
-            if rad <= min_distance:
+            if rad < min_distance:
                 return True
         for p in pp:
             if p in people_list:
                 rad = len(output_list) - i - 1
-                if rad <= min_distance:
+                if rad < min_distance:
                     return True
+    return False
+
 
 def put_to_end(data_list, index):
+    data_to_put = data_list[index]
     data_list.pop(index)
-    data_list.append(name)
+    data_list.append(data_to_put)
+
 
 for s, df in sheets.items():
-    # data_head = df.head()
-    # data_row = df.iloc()
-    # d = data_head.values
-    # ship_names = list(map(lambda a: a[0], data_head.values.tolist()))
-    # ships_people = data_head["ФИО уастников"].to_list()
-    ship_names_first = ["Лодка 1",
-                        "Лодка 2",
-                        "Лодка 5",
-                        "Лодка 2",
-                        "Лодка 2",
-                        "Лодка 3",
-                        "Лодка 4",
-                        "Лодка 3",
-                        "Лодка 4",
-                        ]
-
-    ships_people_first = ["Андрей Андреев;Иван Иванович;Кирил Кириллович",
-                          "Михаил Михайлович;Арсений Арсеньеви;Илья Ильич",
-                          "Вадим Вадимович;Иван Иванович",
-                          "Андрей Андреев;Иван Иванович;Кирил Кириллович",
-                          "Андрей Андреев;Иван Иванович;Кирил Кириллович",
-                          "Михаил Михайлович;Асений Арсеньеви;Илья Ильич",
-                          "Андрей Андреев;Иван Иванович;Кирил Кириллович",
-                          "Михаил Михайлович;Арсений Арсеньеви;Илья Ильич",
-                          "Михаил Михайлович;Асений Арсеньеви;Илья Ильич",
-                          ]
-    ship_names = ship_names_first.copy()
-    ships_people = ships_people_first.copy()
+    data = original_data.copy()
     min_rad = 1
     permutation_count = 0
-    while len(out) < len(ship_names):
-        ship_name = ship_names[0]
-        people = ships_people[0]
-        people_list = people.split(";")
-        if permutation_count > len(ship_names):
-            ship_names = ship_names_first.copy()
-            ships_people = ships_people_first.copy()
-            put_to_end(ship_names, 0)
-            put_to_end(ships_people, 0)
-            permutation_count = 0
-        is_colliding = check_collide(out, ship_name, people_list, min_rad)
-        if is_colliding:
-            permutation_count += 1
-            continue
+    no_vars = False
+    while True:
+        while len(out) < len(original_data):
+            ship_name, people = data[0]
+            people_list = people.split(";")
+            if permutation_count > len(data):
 
-        out.append((ship_name, people_list))
-        ship_names.remove(ship_name)
-        ships_people.remove(people)
+                # data = original_data.copy()
+                put_to_end(data, 0)
+                permutation_count = 0
+                no_vars += 1
+            if no_vars > 3:
+
+                data = original_data.copy()
+                put_to_end(data, 0)
+                out.clear()
+                permutation_count = 0
+                min_rad -= 1
+                break
+            is_colliding = check_collide(out, ship_name, people_list, min_rad)
+            if is_colliding:
+                put_to_end(data, 0)
+                permutation_count += 1
+                continue
+
+            out.append(data[0])
+            data.pop(0)
+            permutation_count = 0
+        if len(out) == len(original_data) or min_rad < 1:
+            break
+        data = original_data.copy()
+        put_to_end(data, 0)
+        out.clear()
         permutation_count = 0
+        min_rad -= 1
 
 print(out)
